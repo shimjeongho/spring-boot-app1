9cdd93efb300b21b7ce47dbd874cabb856523abc
shimjeongho <shimjh0628@gmail.com>
2025-07-04 15:19:30 +0900

    first commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,11 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		return "index";
+	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helo() {
+		return "hello";
 	}
 }
